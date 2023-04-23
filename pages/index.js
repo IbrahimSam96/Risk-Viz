7413b3742e915313212b68385e858b516fa99552
Ibrahimsam96 <Ibrahimsam96@gmail.com>
2023-04-23 21:52:36 +0300

    Added mui-datatable

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 
 import dynamic from 'next/dynamic'
+import Table from '../components/Tables';
 
 const DynamicMap = dynamic(() => import('../components/Map'), {
     ssr: false,
@@ -12,6 +13,11 @@ const Home = () => {
         <div className={`h-full w-full min-h-screen grid grid-cols-[repeat(7,1fr)] grid-rows-[50px,650px,250px,auto] bg-[white] `} >
             <div className={`col-start-1 col-end-4 row-start-2 row-end-3 `}>
                 <DynamicMap />
+                
+            </div>
+
+            <div className={`col-start-1 col-end-8 row-start-3 row-end-4 mx-4 my-4 `}>
+                <Table />
                 
             </div>
 
