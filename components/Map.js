0361809ea22889754c19939a044497b009d6c83d
Ibrahimsam96 <Ibrahimsam96@gmail.com>
2023-04-25 19:25:40 +0300

    Optimized Chart Component

--- components/Map.js
+++ components/Map.js
@@ -1,75 +1,44 @@
-import { LayersControl, MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+// React
 import { useEffect, useState } from "react";
-import GSheetReader from 'g-sheets-api'
-
-
-import { icon } from "leaflet"
+// CTX
+import { useData } from '../DataFetcher';
+// Leaflet
 import L from 'leaflet'
-import * as SVGMarker from '../components/Markers'
 import 'leaflet/dist/leaflet.css';
-import { FormControl, InputLabel, MenuItem, NativeSelect, Select } from '@mui/material';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+// Extends leaflet functionality to support SVG markers
+import * as SVGMarker from '../components/Markers'
 
-const Map = () => {
 
-    const [data, setData] = useState([]);
-    const [legend, setLegend] = useState([]);
+const Map = ({ activeYear }) => {
 
-    const [activeYear, setActiveYear] = useState("")
+    const Data = useData();
+    // Used for generating svg marker color scale 
     const [min, setMin] = useState()
     const [max, setMax] = useState()
 
-
     useEffect(() => {
 
-        const options = {
-            apiKey: process.env.NEXT_PUBLIC_GOOGLE_SHEETS_API,
-            sheetId: '1Y_yiT-_7IimioBvcqiCPwLzTLazfdRyzZ4k3cpQXiAw',
-            sheetNumber: 1,
-            sheetName: 'sample_data', // if sheetName is supplied, this will take precedence over sheetNumber
-            returnAllResults: true,
+        if (Data.length > 0) {
+            // Calculates risk rating max and min values
+            let max = Data[0]["Risk Rating"];
+            let min = Data[0]["Risk Rating"];
+
+            for (let i = 1; i < Data.length; i++) {
+                if (Data[i]["Risk Rating"] > max) {
+                    max = Data[i]["Risk Rating"];
+                }
+                if (Data[i]["Risk Rating"] < min) {
+                    min = Data[i]["Risk Rating"];
+                }
+            }
+            setMax(max)
+            setMin(min)
         }
 
-        GSheetReader(
-            options,
-            results => {
-                setData(results)
-                // console.log(results)
+    }, [Data])
 
-                // Calculate max and min for markers color scale 
-                let max = results[0]["Risk Rating"];
-                let min = results[0]["Risk Rating"];
-
-                for (let i = 1; i < results.length; i++) {
-                    if (results[i]["Risk Rating"] > max) {
-                        max = results[i]["Risk Rating"];
-                    }
-                    if (results[i]["Risk Rating"] < min) {
-                        min = results[i]["Risk Rating"];
-                    }
-                }
-
-                setMax(max)
-                setMin(min)
-
-                // Set up legend i,e, List of unique years
-                const hash = {};
-
-                for (let i = 0; i < results.length; i++) {
-                    hash[results[i]["Year"]] = true;
-                }
-
-                const uniqueArr = Object.keys(hash).map(Number);
-
-                console.log(uniqueArr);
-                setLegend(uniqueArr);
-
-            },
-            error => {
-                // OPTIONAL: handle errors here
-            });
-
-    }, [])
-
+    // Generate color scale based on risk rating
     function generateColorScale(minValue, maxValue, value) {
         // Calculate the range of values
         var range = maxValue - minValue;
@@ -78,81 +47,75 @@ const Map = () => {
         var valuePosition = (value - minValue) / range;
 
         // Calculate the RGB values for the corresponding color
-        var red = Math.floor((1 - valuePosition) * 255);
-        var green = Math.floor(valuePosition * 255);
+        var green = Math.floor((1 - valuePosition) * 255);
+        var red = Math.floor(valuePosition * 255);
         var blue = 0;
 
         // Return the RGB color as a string
-        return "rgb(" + red + "," + green + "," + blue + ")";
+        return "rgb(" + red + "," + green + "," + blue + ")"
+
     };
 
     return (
-        <span >
-            {legend.length > 0 ?
-                <span className={`flex justify-end`}>
-                    <p className={`inline text-[#1976D2] text-sm mx-4 whitespace-nowrap my-auto`}>Select Year</p>
-                    <FormControl >
-                        <Select
-                            value={activeYear}
-                            onChange={(v) => {
-                                setActiveYear(v.target.value)
-                                console.log(v.target.value)
-                            }}
-                        >
-                            {legend.map((value) => {
-                                return (
-                                    <MenuItem key={value} value={value}>{value}</MenuItem>
-                                )
-                            })}
+        <MapContainer center={Data.length > 0 ? [Data[0]['Lat'], Data[0]['Long']] : [46.1351, -60.1831]} zoom={3} scrollWheelZoom={true} className={`w-[100%] h-[100%]`}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
 
-                        </Select>
-                    </FormControl>
+            {Data.map((obj) => {
 
-                </span>
-                :
-                <span className={`flex ml-auto p-6 bg-[grey] w-[25%] h-[35px] rounded animate-pulse `}>  </span>
-            }
+                let color = generateColorScale(min, max, obj["Risk Rating"])
 
-            <MapContainer center={[51.505, -0.09]} zoom={3} scrollWheelZoom={false} className={`w-[100%] h-[100%]`}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
+                let options = {
+                    icon: 'leaf',
+                    iconShape: 'marker',
+                    backgroundColor: color,
+                    borderColor: "white"
+                };
+                // Returns markers with the same year property as selectedYear
+                // NOTE: We could use a UID generator library instead of using obj properties for unique key 
+                if (obj['Year'] == activeYear) {
+                    let factorsKeys = Object.keys(JSON.parse(obj['Risk Factors']));
+                    let factorsValues = Object.values(JSON.parse(obj['Risk Factors']));
+                    let tooltip = ``
 
-                {data.map((obj) => {
+                    for (let k = 0; k < factorsKeys.length; k++) {
+                        tooltip += factorsKeys[k] + `: ` + factorsValues[k] + `
+                        `
+                    }
 
-                    let options = {
-                        icon: 'leaf',
-                        iconShape: 'marker',
-                        backgroundColor: generateColorScale(min, max, obj["Risk Rating"]),
-                        borderColor: "white"
-                    };
-
-                    if (Number(obj['Year']) == activeYear) {
-                        return (
-                            <Marker key={obj["Asset Name"]
-                                + obj["Lat"] +
+                    return (
+                        <Marker
+                            key={
+                                obj["Asset Name"] +
+                                obj["Lat"] +
                                 obj["Long"] +
                                 obj["Risk Rating"] +
                                 obj["Business Category"] +
                                 obj["Year"] +
-                                obj["Risk Factors"]}
-                                icon={L.BeautifyIcon.icon(options)}
-                                position={[obj["Lat"], obj["Long"]]}
-                            >
-                                <Popup>
-                                    {obj["Asset Name"]} - <br /> {obj["Business Category"]}
-                                </Popup>
-                            </Marker>
-                        )
-                    }
+                                obj["Risk Factors"]
+                            }
+                            icon={L.BeautifyIcon.icon(options)}
+                            position={[obj["Lat"], obj["Long"]]}
+                        >
+                            <Popup>
+                                <p className={`inline`}> {obj["Asset Name"]} - {obj["Business Category"]} </p>
+                                <p className={`inline`}> { `(${obj["Year"]})` }  </p>
+                                <br />
+                                <br />
+                                <p style={{ color: color }} className={`inline whitespace-nowrap`}> Risk Rating: {obj["Risk Rating"]} </p>
+                                <br />
+                                <br />
+                                <p className={`inline whitespace-pre-line`}>{tooltip}</p>
+                            </Popup>
+                        </Marker>
+                    )
+                }
 
-                })}
+            })}
 
-            </MapContainer>
-
-
-        </span>
+        </MapContainer>
     )
 }
 export default Map;
